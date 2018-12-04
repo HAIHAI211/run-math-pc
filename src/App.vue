648634979e4cd1b2e4135027bd3611053d6a34b0
harrison <851596652@qq.com>
2018-12-04 15:09:29 +0800

    666

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,78 @@
 <template>
-  <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <router-view/>
-  </div>
+  <el-container id="app">
+    <el-header class="top-nav">
+      <span class="title">极客社群营销平台</span>
+    </el-header>
+    <el-container>
+      <el-aside class="left-side">
+        <el-menu
+          default-active="2"
+          class="menu"
+          @open="_openMenu"
+          @close="_closeMenu"
+          text-color="#fff"
+          background-color="#545c64"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <span>小程序</span>
+            </template>
+            <!--<el-menu-item-group>-->
+              <!--<template slot="title">分组一</template>-->
+              <!--<el-menu-item index="1-1">选项1</el-menu-item>-->
+              <!--<el-menu-item index="1-2">选项2</el-menu-item>-->
+            <!--</el-menu-item-group>-->
+            <!--<el-menu-item-group title="分组2">-->
+              <!--<el-menu-item index="1-3">选项3</el-menu-item>-->
+            <!--</el-menu-item-group>-->
+            <el-submenu index="1-4">
+              <template slot="title">步好玩</template>
+              <el-menu-item index="1-4-1">规则</el-menu-item>
+              <el-menu-item index="1-4-2">礼品</el-menu-item>
+              <el-menu-item index="1-4-3">广告</el-menu-item>
+              <el-menu-item index="1-4-4">订单</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <span>公众号</span>
+            </template>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main><router-view/></el-main>
+    </el-container>
+  </el-container>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    _openMenu () {},
+    _closeMenu () {}
+  }
 }
 </script>
-
-<style>
+<style lang="stylus" scoped>
+@import "~@/assets/style/variable.styl"
 #app {
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
+  .top-nav{
+    /*border-bottom 1px solid #eee*/
+    background $main-color
+    display flex
+    align-items center
+    .title{
+      color #fff
+      font-size 23px
+      font-weight 300
+      letter-spacing 1px
+      transform scale(1, 1.25)
+    }
+  }
+  .left-side{
+    .menu{
+      height 100%
+    }
+  }
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,7 @@
 <template>
-  <!--<div class="page index-page">-->
-
-  <!--</div>-->
-  <el-container class="page index-page">
-    <el-header>上</el-header>
-    <el-container>
-      <el-aside>左</el-aside>
-      <el-main>主</el-main>
-    </el-container>
-  </el-container>
+  <div class="page index-page">
+    首页
+  </div>
 </template>
 <script>
 </script>
