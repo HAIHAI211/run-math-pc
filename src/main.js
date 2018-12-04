cb4880c24df2bb851810f90b5029f5cfbc26b41f
harrison <851596652@qq.com>
2018-12-04 11:39:50 +0800

    666

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import '@/assets/style/index.styl'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="index-page">
-    首页
-  </div>
+  <!--<div class="page index-page">-->
+
+  <!--</div>-->
+  <el-container class="page index-page">
+    <el-header>上</el-header>
+    <el-container>
+      <el-aside>左</el-aside>
+      <el-main>主</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 </script>
