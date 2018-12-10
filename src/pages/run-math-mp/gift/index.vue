ae2c4c1ee54218d1d8b0c08dfd6d3c951a396700
harrison <851596652@qq.com>
2018-12-10 12:48:17 +0800

    666

--- src/pages/run-math-mp/gift/index.vue
+++ src/pages/run-math-mp/gift/index.vue
@@ -14,7 +14,7 @@
           <el-dropdown-item>实物</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>-->
-      <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tabs v-model="activeName">
         <el-tab-pane label="文档礼品" name="doc">
           <el-table
             :data="tableData"
@@ -54,13 +54,90 @@
             </el-table-column>
           </el-table>
         </el-tab-pane>
-        <el-tab-pane label="视频礼品" name="video">视频礼品</el-tab-pane>
-        <el-tab-pane label="实物礼品" name="real">实物礼品</el-tab-pane>
+        <el-tab-pane label="视频礼品" name="video">
+          <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+            <el-table-column
+              prop="index"
+              label="序号"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="礼品名称"
+              width="400">
+            </el-table-column>
+            <el-table-column
+              prop="presentType"
+              label="类型"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="uploadTime"
+              label="上传时间"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="totalAmount"
+              label="剩余数量"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              label="操作">
+              <template slot-scope="scope">
+                <el-button @click="_edit(scope.row)" type="text" size="small">编辑</el-button>
+                <el-button @click="_delete(scope.row)" type="text" size="small">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="实物礼品" name="real">
+          <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+            <el-table-column
+              prop="index"
+              label="序号"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="礼品名称"
+              width="400">
+            </el-table-column>
+            <el-table-column
+              prop="price"
+              label="价格(数学币)"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="uploadTime"
+              label="上传时间"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="totalAmount"
+              label="剩余数量"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              label="操作">
+              <template slot-scope="scope">
+                <el-button @click="_edit(scope.row)" type="text" size="small">编辑</el-button>
+                <el-button @click="_delete(scope.row)" type="text" size="small">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
       </el-tabs>
       <el-pagination
         background
         class="pagination"
         layout="prev, pager, next"
+        :current-page.sync="pageNum"
         :page-count="pageCount" @current-change="_currentChange">
       </el-pagination>
     </el-card>
@@ -81,15 +158,13 @@ export default {
     activeName: {
       async handler (newV, oldV) {
         console.log('activeName从' + oldV + '变成了' + newV)
+        this.pageNum = 1
         await this._fetchGifts()
       },
       immediate: true
     }
   },
   methods: {
-    handleClick (tab, event) {
-      console.log(tab, event)
-    },
     _edit (e) {
       console.log(e)
     },
@@ -103,14 +178,20 @@ export default {
       }
     },
     async _fetchGifts () {
+      let apiName = ''
       if (this.activeName === 'doc') {
-        const result = await this.api.runMathMp.getDocGifts(this.pageNum)
-        console.log('pageCount', result.pageCount)
-        this._addIndex(result)
-        this.tableData = result.data
-        this.pageCount = result.pageCount
-        console.log(result)
+        apiName = 'getDocGifts'
+      } else if (this.activeName === 'video') {
+        apiName = 'getVideoGifts'
+      } else {
+        apiName = 'getRealGifts'
       }
+      const result = await this.api.runMathMp[apiName](this.pageNum)
+      console.log('pageCount', result.pageCount)
+      this._addIndex(result)
+      this.tableData = result.data
+      this.pageCount = result.pageCount
+      console.log(result)
     },
     _currentChange (currentPage) {
       console.log('currentChange', currentPage)
