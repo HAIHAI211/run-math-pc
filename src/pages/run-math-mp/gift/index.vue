28ab017647399e6ce8e41ae3309623f5d1614d47
harrison <851596652@qq.com>
2018-12-10 09:44:24 +0800

    666

--- src/pages/run-math-mp/gift/index.vue
+++ src/pages/run-math-mp/gift/index.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="page gift-page">
-    礼品
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
+export default {
+  data () {
+    return {
+      activeName: 'second'
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+  }
+}
 </script>
 <style lang="stylus" scoped>
   @import "~@/assets/style/variable.styl"
